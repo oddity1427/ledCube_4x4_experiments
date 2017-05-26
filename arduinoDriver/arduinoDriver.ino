4df31fc00882279ea9bd798eed29aa3b7e04f837
Oddity1427 <wolfornd@miamioh.edu>
2017-05-26 15:04:10 -0400

    Completed the Arduino code, iterates through the leds to prove they are all working and usable by the arduino. does not support multiple led 'frames, but may implement this is the avr version of the code, just not necessary for this iteration'

--- arduinoDriver/arduinoDriver.ino
+++ arduinoDriver/arduinoDriver.ino
@@ -17,14 +17,17 @@
 #define c_1_1 7
 #define c_1_2 4
 #define c_1_3 1
-#define c_2_0 A1
-#define c_2_1 A4
+#define c_2_0 15
+#define c_2_1 18
 #define c_2_2 3
 #define c_2_3 0
-#define c_3_0 A0
-#define c_3_1 A2
-#define c_3_2 A3
-#define c_3_3 A5
+#define c_3_0 14
+#define c_3_1 16
+#define c_3_2 17
+#define c_3_3 19
+
+int layers[] = { h_0, h_1, h_2, h_3 };
+int columns[4][4] = {{c_0_0, c_0_1, c_0_2, c_0_3} , {c_1_0, c_1_1, c_1_2, c_1_3 },{ c_2_0, c_2_1, c_2_2, c_2_3 }, {c_3_0, c_3_1, c_3_2, c_3_3}};
 
 //delay between turning on and turning off sets of LEDs, meant to be adjusted to suit the speed that you want the animations to play at
 int stepDelay =  250;
@@ -32,39 +35,47 @@ int stepDelay =  250;
 
 void setup() {
   // need to set all of the pins so that they can source or sink current
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(A4, OUTPUT);
-  pinMode(A5, OUTPUT);
+  for(int i = 0; i < 20; i++){
+    pinMode(i, OUTPUT);
+  }
 
+  //set the pins to the initial off position
+  for(int i = 0; i < 4; i++){
+    digitalWrite(layers[i], LOW);
+  }
+  for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 4; j++){
+      digitalWrite(columns[i][j], HIGH);
+    }
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //turn on all of the leds individually and sequentially for demo purposes
+  for(int i = 0; i <4; i++){
+    for(int j = 0; j < 4; j++){
+      for(int k = 0; k < 4; k++){
+        mapLED(i, j, k, true);
+        delay(stepDelay);
+        mapLED(i, j, k, false);
+      }
+    }
+  }
+
 
 }
 
 //this has to have a really long if structure because the analog pins cannot be put into an int array
 //really annoying but fine
 void mapLED(int x, int y, int z, bool on){
-  
+  if(on){
+    digitalWrite( columns[x][y], LOW);
+    digitalWrite(layers[z], HIGH);
+  }else{
+    digitalWrite( columns[x][y], HIGH);
+    digitalWrite(layers[z], LOW);
+  }
 }
 
 
