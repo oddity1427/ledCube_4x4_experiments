4034314f9de7aeddb743071018a90a1eff7a1906
Oddity1427 <wolfornd@miamioh.edu>
2017-05-26 00:05:10 -0400

    minor changes to the arduino script, started pin definitions, commiting to update changes before sleeping

--- arduinoDriver/arduinoDriver.ino
+++ arduinoDriver/arduinoDriver.ino
@@ -1,3 +1,32 @@
+
+//the anodes of the LEDs are connected together to form the layers of the cube
+//the cathodes are chained together to form a square of 16 columns
+
+//definitions for the pins going to the anode layers
+#define h_0 10
+#define h_1 11
+#define h_2 12
+#define h_3 13
+
+//definitions fot the pins going to the columns
+#define c_0_0 8
+#define c_0_1 6
+#define c_0_2 5
+#define c_0_3 3
+#define c_1_0 9
+#define c_1_1 7
+#define c_1_2 4
+#define c_1_3 2
+#define c_2_0 A1
+#define c_2_1 A4
+#define c_2_2 0
+#define c_2_3 0
+#define c_3_0 0
+#define c_3_1 0
+#define c_3_2 0
+#define c_3_3 0
+
+
 void setup() {
   // put your setup code here, to run once:
 
